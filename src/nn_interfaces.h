d049dc85f3bb8933ef7ad084e859f9f41cdb0b3e
ana17hy <ana.huapaya@utec.edu.pe>
2025-06-30 00:32:34 -0500

    listo tensor.h :D

--- src/nn_interfaces.h
+++ src/nn_interfaces.h
@@ -1,8 +1,42 @@
-//
-// Created by Ana Huapaya on 30/06/25.
-//
 
-#ifndef NN_INTERFACES_H
-#define NN_INTERFACES_H
+#ifndef PROG3_NN_FINAL_PROJECT_V2025_01_LAYER_H
+#define PROG3_NN_FINAL_PROJECT_V2025_01_LAYER_H
 
-#endif //NN_INTERFACES_H
+#include "tensor.h"
+
+namespace utec::neural_network {
+
+    template<typename T, size_t DIMS>
+    using Tensor = utec::algebra::Tensor<T, DIMS>;
+
+    // Interfaz del optimizador (SGD o Adam)
+    template<typename T>
+    struct IOptimizer {
+        virtual ~IOptimizer() = default;
+        virtual void update(Tensor<T,2>& params, const Tensor<T,2>& gradients) = 0;
+        virtual void step() {}
+    };
+
+    // Interfaz de las capas (Dense y los diferentes tipos de activación)
+    template<typename T>
+    struct ILayer {
+        virtual ~ILayer() = default;
+        virtual Tensor<T,2> forward(const Tensor<T,2>& x) = 0;
+        virtual Tensor<T,2> backward(const Tensor<T,2>& gradients) = 0;
+
+        // Se utiliza para actualizar los parameters a través el optimizador
+        // Se puede llamar tanto el método update y step si es requerido
+        virtual void update_params(IOptimizer<T>& optimizer) {}
+    };
+
+    // Interfaz de las perdidas (MSE o BCE)
+    template<typename T, size_t DIMS>
+    struct ILoss {
+        virtual ~ILoss() = default;
+        virtual T loss() const = 0;
+        virtual Tensor<T,DIMS> loss_gradient() const = 0;
+    };
+
+}
+
+#endif //PROG3_NN_FINAL_PROJECT_V2025_01_LAYER_H
--- src/nn_loss.h
+++ src/nn_loss.h
@@ -1,8 +1,65 @@
-//
-// Created by Ana Huapaya on 30/06/25.
-//
 
-#ifndef NN_LOSS_H
-#define NN_LOSS_H
+#ifndef PROG3_NN_FINAL_PROJECT_V2025_01_LOSS_H
+#define PROG3_NN_FINAL_PROJECT_V2025_01_LOSS_H
 
-#endif //NN_LOSS_H
+#include "nn_interfaces.h"
+#include <cmath>
+
+namespace utec {
+    namespace neural_network {
+
+        // MSE: mide que tan lejos están las predicciones de los valores reales
+        template<typename T>
+        class MSELoss final : public ILoss<T, 2> {
+            Tensor<T,2> pred, real;
+        public:
+            MSELoss(const Tensor<T,2>& y_pred, const Tensor<T,2>& y_true)
+              : pred(y_pred), real(y_true) {}
+
+            T loss() const override {
+                T total = 0;
+                for (size_t i = 0; i < pred.size(); ++i)
+                    total += (pred[i] - real[i]) * (pred[i] - real[i]);
+                return total / pred.size();
+            }
+
+            Tensor<T,2> loss_gradient() const override {
+                Tensor<T,2> grad = pred;
+                for (size_t i = 0; i < grad.size(); ++i)
+                    grad[i] = 2 * (pred[i] - real[i]) / pred.size();
+                return grad;
+            }
+        };
+
+        // BCE: se usa para problemas de clasificación con salida 0 o 1
+        template<typename T>
+        class BCELoss final : public ILoss<T, 2> {
+            Tensor<T,2> yhat, y;
+            static constexpr T epsilon = 1e-7;
+        public:
+            BCELoss(const Tensor<T,2>& y_pred, const Tensor<T,2>& y_true)
+              : yhat(y_pred), y(y_true) {}
+
+            T loss() const override {
+                T sum = 0;
+                for (size_t i = 0; i < yhat.size(); ++i) {
+                    T p = std::clamp(yhat[i], epsilon, T(1) - epsilon);  // evitar log(0)
+                    sum += -y[i]*std::log(p) - (1 - y[i])*std::log(1 - p);
+                }
+                return sum / yhat.size();
+            }
+
+            Tensor<T,2> loss_gradient() const override {
+                Tensor<T,2> grad = yhat;
+                for (size_t i = 0; i < grad.size(); ++i) {
+                    T p = std::clamp(yhat[i], epsilon, T(1) - epsilon);
+                    grad[i] = (p - y[i]) / (p * (1 - p)) / yhat.size();  // derivada de BCE
+                }
+                return grad;
+            }
+        };
+
+    }
+}
+
+#endif //PROG3_NN_FINAL_PROJECT_V2025_01_LOSS_H
